6c40427b1ff106cc0955c4b50fd85bd486ad4d26
Parth <itsparth20@gmail.com>
2016-12-15 02:07:07 -0800

    Heaters Coding Program using Binary Search Array
    LeetCode

--- LeetCode/Heaters.java
+++ LeetCode/Heaters.java
@@ -5,10 +5,33 @@
  */
 package LeetCode;
 
+import java.util.Arrays;
+
 /**
  *
  * @author itspa
  */
 public class Heaters {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        int result = 0;
+        for (int house : houses) {
+            int index = Arrays.binarySearch(heaters, house);
+            if (index < 0) {
+                index = ~index;
+                int dist1 = index - 1 >= 0 ? house - heaters[index - 1] : Integer.MAX_VALUE;
+                int dist2 = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+
+                result = Math.max(result, Math.min(dist1, dist2));
+            }
+        }
+        return result;
+    }
     
+    public static void main(String[] args){
+        int[] h = {1,2,3,4,7};
+        int[] t = {1,6};        
+        Heaters hobj = new Heaters();
+        System.out.println(hobj.findRadius(h, t));
+    }
 }
