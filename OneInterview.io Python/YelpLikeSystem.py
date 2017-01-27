c9ae5197ba723ac3b29989865d5211a5d3f89953
Parth <itsparth20@gmail.com>
2017-01-26 22:47:30 -0800

    Yelp Like System Program.
    https://www.oneinterview.io/sample
    
    Build a simple Yelp-like system: Given a set of restaurant and metadata
    (coordinates, ratings, opening hours), design and implement the
    following functionalities without using a database.
    
    1. Find restaurants within specified radius, given a coordinate
    2. Improve the above function by only returning restaurants that are
    open given desired dining hour
    3. Improve the above function by sorting the results by average ratings

--- OneInterview.io Python/YelpLikeSystem.py
+++ OneInterview.io Python/YelpLikeSystem.py
@@ -3,7 +3,13 @@ import sys
 import os
 import cmath
 
+'''
+Build a simple Yelp-like system: Given a set of restaurant and metadata (coordinates, ratings, opening hours), design and implement the following functionalities without using a database.
 
+1. Find restaurants within specified radius, given a coordinate
+2. Improve the above function by only returning restaurants that are open given desired dining hour
+3. Improve the above function by sorting the results by average ratings
+'''
 
 class Yelp(object):
 
