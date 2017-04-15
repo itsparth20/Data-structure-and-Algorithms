9ff3e479c2cbca1b8b4961f053318780ed907d14
Parth <itsparth20@gmail.com>
2017-04-15 12:01:19 -0700

    Added Remove Peek and Bubble down function.
    If heap is 0 then it is min heap.
    If heap is 1 then it is max heap.

--- Practice/TreeGraphLinkedList/Heap.java
+++ Practice/TreeGraphLinkedList/Heap.java
@@ -5,14 +5,21 @@ import java.util.List;
 
 public class Heap {
 	public static void main(String[] args) {
-		heapActivity heap = new heapActivity(1);
-		
+		heapActivity heap = new heapActivity(0); //"0 = Min Heap" and "1 = MaxHeap"
+
 		heap.insert(4);
 		heap.insert(1);
 		heap.insert(6);
 		heap.insert(5);
 		heap.insert(2);
 		heap.insert(3);
+//		heap.removePeek();
+//		heap.removePeek();
+//		heap.removePeek();
+//		heap.removePeek();
+//		heap.removePeek();
+//		heap.removePeek();
+//		heap.removePeek();
 		heap.print();
 	}
 }
@@ -21,8 +28,8 @@ public class Heap {
 // n = n2+1, n2+2;
 // (n-1)2 (n-2)/2
 class heapActivity {
-	private int heapType;
-	private List<Integer> list;
+	private final int heapType;
+	private final List<Integer> list;
 
 	/**
 	 * @param heapType
@@ -40,34 +47,51 @@ class heapActivity {
 			bubbleUp(list.size() - 1);
 		}
 	}
-	
-	public void removePeek(int number){
-		swap(0,list.size()-1);
-		bubbleDown(0);
+
+	public void removePeek() {
+		if (!list.isEmpty()) {
+			swap(0, list.size() - 1);
+			list.remove(list.size() - 1);
+			bubbleDown(0);
+		}
 	}
 
 	private void bubbleDown(int i) {
-		int childOne = i*2+1, childTwo = i*2+2;
-//		while(
-//				(
-//						heapType == 0 &&
-//				(
-//						(childOne < list.size() && list.get(i)>list.get(childOne)) || 
-//						(childTwo < list.size() && list.get(i)>list.get(childTwo))				
-//				)
-//				) 
-//				|| 
-//				(
-//						heapType == 1 && 
-//				(
-//						(childOne < list.size() && list.get(i)<list.get(childOne)) || 
-//						(childTwo < list.size() && list.get(i)<list.get(childTwo))
-//				)
-//				)
-//				){
-//				
-//			
-//		}
+		int childOne = i * 2 + 1, childTwo = i * 2 + 2, exchangeIndex = 0;
+
+		while ((childOne < list.size() && ((heapType == 0 && list.get(childOne) < list.get(i))
+				|| (heapType == 1 && list.get(childOne) > list.get(i))))
+				|| (childTwo < list.size() && ((heapType == 0 && list.get(childTwo) < list.get(i))
+						|| (heapType == 1 && list.get(childTwo) > list.get(i))))) {
+			if (childOne < list.size()) {
+				if (childTwo < list.size()) {
+					if (heapType == 0 && (list.get(i) > list.get(childTwo) || list.get(i) > list.get(childOne))) {
+						exchangeIndex = list.get(childOne) > list.get(childTwo) ? childTwo : childOne;
+					}
+					if (heapType == 1 && (list.get(i) < list.get(childOne) || list.get(i) < list.get(childTwo))) {
+						exchangeIndex = list.get(childOne) > list.get(childTwo) ? childOne : childTwo;
+					}
+				} else {
+					if (heapType == 0 && (list.get(i) > list.get(childOne))) {
+						exchangeIndex = childOne;
+					}
+					if (heapType == 1 && (list.get(i) < list.get(childOne))) {
+						exchangeIndex = childOne;
+					}
+				}
+			} else if (childTwo < list.size()) {
+				if (heapType == 0 && (list.get(i) > list.get(childTwo))) {
+					exchangeIndex = childTwo;
+				}
+				if (heapType == 1 && (list.get(i) < list.get(childTwo))) {
+					exchangeIndex = childTwo;
+				}
+			}
+			swap(exchangeIndex, i);
+			i = exchangeIndex;
+			childOne = i * 2 + 1;
+			childTwo = i * 2 + 2;
+		}
 	}
 
 	public void print() {
