c05196cb8f38b4d3c46b170450f54cfb82496a93
Parth <itsparth20@gmail.com>
2017-09-21 00:13:34 -0700

    Remove odd id node

---
+++ CodingChallenge/Vendor/RemoveOddIdNode.java
@@ -0,0 +1,23 @@
+static LinkedListNode deleteEven(LinkedListNode list) {
+        LinkedListNode head = null;
+        LinkedListNode temp = null;
+        while(list != null){
+            if(head == null && list.val%2 == 1){
+                head = list;
+                temp = head;
+            }
+             
+            
+            else{
+                if(list.val%2 == 0){
+                    temp.next = list;
+                    temp = temp.next;
+                }
+            }
+            list = list.next;
+            
+        }
+        temp.next = null;
+        
+        return head;
+    }
