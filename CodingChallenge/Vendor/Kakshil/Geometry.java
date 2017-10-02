a625f092640b9e37f0bc707a44c5a8dac5f27788
Parth <itsparth20@gmail.com>
2017-10-01 18:11:01 -0700

    Commit

--- CodingChallenge/Vendor/Kakshil/Geometry.java
+++
@@ -1,9 +0,0 @@
-class Geometry implements Rectangle, Circle{
-    public int area(int height, int width){
-        return height*width;
-    }
-    public double area(int radius){
-        return 3.14*radius*radius;
-    }
-}
-
