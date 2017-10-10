24e66400478882a3be7d9b58fcafb69eb28cf23e
Parth <itsparth20@gmail.com>
2017-10-09 18:38:57 -0700

    Improved Find Peak program.

--- Java_8/FindPeak.java
+++ Java_8/FindPeak.java
@@ -7,23 +7,24 @@ import java.util.Map;
 import java.util.OptionalDouble;
 
 public class FindPeak {
-	static Map<String, List<Double>> map = new HashMap<>();
+	Map<String, List<Double>> state = new HashMap<>();
 
+    public void accumulate(String idx, String spaceId, double v, double w) {
+        state.putIfAbsent(idx, new ArrayList<>());
+        List<Double> list = state.get(idx);
+            list.add(v*w);
+            list.add(w);
+	}
+
+    public double result() {
+
+        OptionalDouble peak = state.entrySet().stream()
+									.mapToDouble(t -> t.getValue().get(1) != 0 ? t.getValue().get(0)/t.getValue().get(1): 0)
+									.max();
+
+		return peak.isPresent()?peak.getAsDouble()*100:0;
+
+    }
 	public static void main(String[] args) {		
 	}
-	
-	//Calculate map
-    public static void accumulate(String idx, String spaceId, double v, double w) {
-        map.putIfAbsent(idx, new ArrayList<Double>(2));
-        List<Double> arr = map.get(idx);
-        arr.add(0, arr.get(0)+(v*w));
-        arr.add(1, arr.get(1)+(w));
-        map.put(idx, arr);  
-    }
-    
-    //get Peak
-    public static Double getPeak(){
-    	OptionalDouble peak = map.entrySet().stream().mapToDouble(t -> t.getValue().get(1) != 0 ? t.getValue().get(0)/t.getValue().get(1) : 0).max();
-    	return peak.isPresent()?peak.getAsDouble()*100:0;
-    }	
 }
