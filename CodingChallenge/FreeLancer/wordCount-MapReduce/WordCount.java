2fbc59feff3b13bff010dbd2aab75a8b3376a0a1
Parth <itsparth20@gmail.com>
2017-10-15 23:41:46 -0700

    Hadoop Map Reduce FreeLancer Work

---
+++ CodingChallenge/FreeLancer/wordCount-MapReduce/WordCount.java
@@ -0,0 +1,89 @@
+package com.program.test.solution.free;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCount {
+
+	public static void main(String[] args) throws Exception {
+
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: wordcount <in> <out>");
+			System.exit(2);
+		}
+
+		@SuppressWarnings("deprecation")
+		Job job = new Job(conf, "word count");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			if(value.toString() == null) return;
+			String val = value.toString().toLowerCase().replace("-", " ");
+			String finalVal = val.replaceAll("[^a-zA-Z0-9 ]", "");
+
+			StringTokenizer itr = new StringTokenizer(finalVal);
+			if (itr.hasMoreTokens()) {
+				if (itr.countTokens() == 1) {
+					String it = itr.nextToken();
+					word.set(it);
+					context.write(word, one);
+				} else {
+					String prev = itr.nextToken();
+					while (itr.hasMoreTokens()) {
+						String next = itr.nextToken();
+						
+						word.set(prev + " " + next);
+						context.write(word, one);
+						if (itr.hasMoreTokens()) {
+							prev = next;
+						}
+					}
+				}
+			}
+
+		}
+	}
+
+	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+}
