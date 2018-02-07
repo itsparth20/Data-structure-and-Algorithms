f9d9dea6c9ba75e909333ddb44a65508997d56b8
Parth <itsparth20@gmail.com>
2018-02-06 16:25:38 -0800

    Change words and palindrom using java 8

--- CodingChallenge/FreeLancer/VisaVendor/Jeeten/ChangeWords.java
+++ CodingChallenge/FreeLancer/VisaVendor/Jeeten/ChangeWords.java
@@ -1,24 +1,37 @@
 package freelance.visa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
 import javax.net.ssl.SSLContext;
 
 public class ChangeWords {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print(getString("visa printer function dgsdfsdfsd"));
+		System.out.println(getString("visa printer function dgsdfsdfsd"));
+		
+		String x = "racecar";
+		IntPredicate p1 = i -> x.charAt(i) == x.charAt(x.length() -1 - i);
+		boolean b = IntStream.range(0, x.length()).allMatch(p1);
+		System.out.println(b);
 	}
 	
 	public static String getString(String s) {
-		StringBuilder sb = new StringBuilder();
-		String[] arr = s.split(" ");
-		for(String str: arr) {
+		StringBuilder sb = new StringBuilder();		
+		Arrays.stream(s.split(" ")).forEach(str -> {
 			sb.append(str.charAt(0));
 			sb.append(str.length()-2);
 			sb.append(str.charAt(str.length()-1));
 			sb.append(" ");
-		}
+		});
+		
 		return sb.toString();
 	}
-
+	
+	
 }
